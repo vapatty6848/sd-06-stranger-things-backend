535f754b0ba46c61c6efc859f6f54bfe32075329
Isa Koga <isadorakoga710@gmail.com>
2021-03-08 15:39:06 -0300

    :rotating_light: linter fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { PORT }  = process.env;
+const { PORT } = process.env;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const { UPSIDEDOWM_MODE } = process.env;
+const { UPSIDEDOWM_MODE } = process.env.UPSIDEDOWM_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
