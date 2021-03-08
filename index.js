31055cd507faee0761405aafd59c2465a1970dd1
Lais Gurgel <laisgurgelf@gmail.com>
2021-03-08 19:05:50 -0300

    teste

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv/config');
-
-const { PORT, UPSIDEDOWN_MODE } = process.env;
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
+
+const { PORT } = process.env;
 
 const app = express();
 
@@ -19,7 +19,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'false';
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
   console.log(`Escutando na porta ${PORT}`);
 });
