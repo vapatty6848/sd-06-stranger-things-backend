abd9226c3b2f0adf8593510d372b5a32d79ed10e
Marcelo Pessini <marceloppessini@gmail.com>
2021-03-08 15:18:21 -0300

    Requisito testando6

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
@@ -16,17 +18,17 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const UPSIDEDOWM_MODE = process.env.UPSIDEDOWM_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWM_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
