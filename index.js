6cd6990817ececb2d309c8661c91f534317a72b5
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-03-08 18:32:34 -0300

    feat: fix port

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Escutando na porta 3000`);
 });
