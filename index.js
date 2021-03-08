09c94bdbfc306c822cf0695001d070df84997f6b
William Gomes <williamjog@hotmail.com>
2021-03-08 18:15:10 -0300

    6th requirement

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+
+console.log('HERE', typeof hereIsTheUpsideDown);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
