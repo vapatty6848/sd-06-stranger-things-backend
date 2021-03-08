85314140370918dc27fea21fd0c212d4ada43d7f
Thales Rangel <thalesrangel.SPASSU@petrobras.com.br>
2021-03-08 17:36:07 -0300

    dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -18,12 +19,12 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const UPSIDEDOWM_MODE = process.env.UPSIDEDOWM_MODE === 'true';
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    UPSIDEDOWM_MODE,
+    hereIsTheUpsideDown,
   );
 
   res.status(200).json(characters);
