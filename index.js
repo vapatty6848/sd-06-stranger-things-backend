4fd9dff60435343f96b6c0f1daf33ddef0feb0a8
ricardorosa-dev <ricardorosa@gmail.com>
2021-03-08 16:27:03 -0300

    Requisito 1 ??

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+// true
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,8 +28,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
-});
+const PORT = process.env.PORT || 3000;
 
-// starting
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
+});
