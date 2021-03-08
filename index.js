1df4cb0e51cb32445b6e8a457f7df9875253bf07
flaviannymaciel <flaviannymaciel@gmail.com>
2021-03-08 15:32:55 -0300

    merge master

--- index.js
+++ index.js
@@ -30,5 +30,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Escutando na porta 3000');
+  console.log(`Escutando na porta ${PORT}`);
 });
