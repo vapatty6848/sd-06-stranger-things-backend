8262c0e2d7b5e6bea940485e82dfcf499b1f8bd9
Matheus Dias <matheuscara2000@gmail.com>
2021-03-09 20:21:58 -0300

    Tasks 1-5

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv')
+
 const express = require('express');
 const cors = require('cors');
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
-const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThing
+sRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
@@ -16,17 +19,23 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
 
-app.get('/', (req, res) => {
+const hereIsTheUpsideDown = 
+process.env.UPSIDESOWN_MODE === true;
+
+app.get('/', (request, response) => {
   const characters = strangerThingsService.search(
-    req.query,
+    request.query,
     hereIsTheUpsideDown,
   );
 
-  res.status(200).json(characters);
+  response.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+
+const appPort = process.env.PORT || 3000;
+
+
+app.listen(appPort, () => {
   console.log('Escutando na porta 3000');
 });
