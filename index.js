315a1bfd9e9150791be5c629f0f467d76bc2b5f1
Alvaro Neto <alvaroshn@gmail.com>
2021-03-09 19:23:15 -0300

    heroku changes bk

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -22,14 +24,14 @@ const hereIsTheUpsideDown = true;
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
 app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+  console.log(`Escutando na porta ${PORT}`);
   console.log(process.env.UPSIDEDOWN_MODE)
   console.log(process.env.GITHUB_USER)
   console.log(process.env.PORT)
