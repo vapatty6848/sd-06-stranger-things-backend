8b4edc33c19c25c406f38d9438d72b31abb56d18
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-03-09 12:42:26 -0300

    deploy hawkins

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -18,7 +19,7 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
