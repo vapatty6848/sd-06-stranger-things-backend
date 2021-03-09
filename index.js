27cd2ae09370ad2a90fdf8c8c26ae07d91d007e7
flaviannymaciel <flaviannymaciel@gmail.com>
2021-03-08 21:06:17 -0300

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv/config');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,8 +16,8 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
-
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+// Gargani no slack: https://trybecourse.slack.com/archives/C016CCMKN9E/p1615241356315900?thread_ts=1615234819.284300&cid=C016CCMKN9E
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
