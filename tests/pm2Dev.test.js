fed8b625f3c8c695bbd8ed2d938bec61cc07a037
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-03-09 18:48:41 -0300

    Requiremente 02 - push 01

--- tests/pm2Dev.test.js
+++ tests/pm2Dev.test.js
@@ -1,6 +1,6 @@
 const { dependencies } = require('../package.json');
 
-describe.skip('Verifica se o modulo pm2 foi instalado no projeto', () => {
+describe('Verifica se o modulo pm2 foi instalado no projeto', () => {
   it('Será validado que o modulo pm2 está instalado nas dependências', () => {
     expect(dependencies).toHaveProperty('pm2');
   });
