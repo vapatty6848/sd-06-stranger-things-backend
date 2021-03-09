b026e67862f5adcfaf587f0381162748871d68eb
ricardorosa-dev <ricardorosa@gmail.com>
2021-03-08 22:42:46 -0300

    linterrrrr

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -27,7 +27,6 @@ app.get('/', (req, res) => {
 
   res.status(200).json(characters);
 });
-
 
 const PORT = process.env.PORT || 3000;
 
