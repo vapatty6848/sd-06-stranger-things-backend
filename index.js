3a0b9e3b9b76315755279b73f6200c0bdc2bd645
Usuario <mguimaraes.carneiro@gmail.com>
2021-03-09 14:12:52 -0300

    Requirement 06 | backend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-// teste dotenv
+
 const { PORT } = process.env;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
