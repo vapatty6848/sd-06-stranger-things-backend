55c9cb4d4a6c905f4a7d9ab0d625c5ddc2b49c68
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-03-10 09:22:34 -0300

    untracked node_modules

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
-console.log(hereIsTheUpsideDown);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
