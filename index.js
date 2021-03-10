397999a84c0f456b6401c5d12c2275f212cbef11
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-03-10 13:17:57 -0300

    bk add PORT e arrumando nome de usuario github

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const { PORT } = process.env;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
