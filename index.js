35b50adb74220d210e8917a87c77613e5314a830
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-03-09 21:01:45 -0300

    :bug: PORT
    corrigido a condicional da porta 3000

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const PORT = process.env.PORT || 3000;
 const app = express();
+const { PORT } = process.env;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
