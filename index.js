7ad36557947fa8e2769c4e6c96eff021ae960b00
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-03-10 08:48:15 -0300

    :art: Branch
    Retornei para a branch para tentar dar um push para o avaliador

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -29,6 +28,7 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
 });
