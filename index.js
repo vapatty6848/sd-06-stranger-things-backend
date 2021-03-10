a7a8cc9fa67fd5bcf92c0604d2acffd6a9b37250
vapatty6848 <vapatty@gmail.com>
2021-03-10 17:54:44 -0300

    adicionando arquivo env que estava no gitignore

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const { PORT = 3000, UPSIDEDOWN_MODE } = process.env;
+const PORT = process.env.PORT || 3000;
+const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE;
 // utilizando valores default para o caso do arquivo de variaveis de ambiente não existir
 // ou faltar uma variavel de ambiente
 
@@ -14,25 +15,18 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
-);
-const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
-);
+const strangerThingsRepository = new StrangerThingsRepository(strangerThingsDataset);
+const strangerThingsService = new StrangerThingsService(strangerThingsRepository);
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-  const characters = strangerThingsService.search(
-    req.query,
-    upsideDown,
-  );
+  const characters = strangerThingsService.search(req.query, upsideDown);
 
   res.status(200).json(characters);
 });
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
-  console.log('varialve ', upsideDown);
+  console.log('varialvel ', upsideDown);
 });
