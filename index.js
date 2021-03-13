e10f48df7ae50051adbc3c456712efacaa4f1752
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-03-13 15:54:23 -0300

    Backend Implementation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
