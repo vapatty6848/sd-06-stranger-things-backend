807fba7b0e87c18dba8c7dd65bcffaf5bdcc53e3
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-03-21 22:27:07 -0300

    correcoes

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
