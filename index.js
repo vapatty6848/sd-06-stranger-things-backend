6dbe0ce7814b8afc81a3811d402ae84d259175b2
HCaetano <hrcgeo@gmail.com>
2021-03-25 10:30:20 -0300

    feat: add challenge 1

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
+const { PORT } = process.env.PORT;
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
@@ -16,7 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
