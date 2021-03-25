e1da141913885c8c1301ade3c231d0385529d5f4
HCaetano <hrcgeo@gmail.com>
2021-03-25 10:39:45 -0300

    fix: change PORT import on index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const { PORT } = process.env.PORT;
+const { PORT } = process.env;
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
