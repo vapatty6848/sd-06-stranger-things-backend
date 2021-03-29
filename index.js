7d4caa5cafd96b3ec4ffe246ffe554ce990ae681
pa-volpin <paulo.volpin@gmail.com>
2021-03-28 21:35:54 -0300

    Ajuste index

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(.listen(process.env.PORT || 3000), () => {
   console.log('Escutando na porta 3000');
 });
