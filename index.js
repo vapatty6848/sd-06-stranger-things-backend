17495b98adf3c0a7572a4708a117e655554b460b
pa-volpin <paulo.volpin@gmail.com>
2021-03-28 21:08:28 -0300

    Deploy back-end Hawkins

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const dotEnvConfig = require('dotenv').config;
+
+dotEnvConfig();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,7 +19,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Escutando na porta 3000');
 });
