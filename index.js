cace6327aa36286189852986bfd23a5da75a5c6f
Pedro Duarte <ikbenpedro@gmail.com>
2021-03-30 16:34:06 -0300

    linter

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 
   res.status(200).json(characters);
 });
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on the door ${PORT}`);
