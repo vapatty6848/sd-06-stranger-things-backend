bb1461fa18acab3088120b343c497f5718cd0dbe
Pedro Duarte <ikbenpedro@gmail.com>
2021-03-30 18:42:31 -0300

    .env

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+console.log(hereIsTheUpsideDown);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
