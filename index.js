21101fed1c2ca5718a080ba2e1b4b13a66c66d00
System Administrator <root@MacBook-Pro.local>
2021-04-09 11:11:55 -0300

    upside-down mode

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const trueEslit = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true' ? trueEslit : false;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
