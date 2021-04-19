117fe8bd73db17a8f9d2b97c254f80c5e079ad75
PAULO LINS <paulolins100@gmail.com>
2021-04-18 21:48:59 -0300

    primeiro deploy

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
-const { PORT, upsidedownMode } = require('./config');
+require("dotenv").config();
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -17,17 +18,20 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = upsidedownMode;
+const { UPSIDEDOWN_MODE } = process.env;
+const { PORT } = process.env;
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
     hereIsTheUpsideDown,
-  );
-
+    );
+    
   res.status(200).json(characters);
 });
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
+  console.log(`Ponta-Cabeça ${hereIsTheUpsideDown}`)
 });
