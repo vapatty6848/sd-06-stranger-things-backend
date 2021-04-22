d4b21cbe9b17301c195d6a73e5d8aa0d13af8837
Gabriel Luiz da Silva <gabrielsilva.adm@gmail.com>
2021-04-22 13:55:00 -0300

    test

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-
-const { PORT, UPSIDEDOWN_MODE } = process.env;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -19,20 +16,17 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = (string) => {
-  if (string === 'true') return true;
-  return false;
-};
+const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown(UPSIDEDOWN_MODE),
+    hereIsTheUpsideDown,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+app.listen(3000, () => {
+  console.log('Escutando na porta 3000');
 });
