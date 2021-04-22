b2f4dbc924ac281ec93a3f81d9527b8e3da05c0a
danilocouto1 <dansantosalfa@gmail.com>
2021-04-22 09:27:56 -0300

    Teste2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv/config');
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
+// teste
 
 const app = express();
 
