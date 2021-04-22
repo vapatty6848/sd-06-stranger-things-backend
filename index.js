091d58b9af0c144d8d54ff5fbb9273e89a9c5786
Samuel <samuelpp.s@hotmail.com>
2021-04-22 09:55:16 -0300

    criando upside down

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === false;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
